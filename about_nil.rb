9aee523967fb0c264419ec1e22f378f89c5cecb9
Wilbur Suero <wilbur.himself@gmail.com>
2013-06-09 03:40:27 -0400

    What message was attached to the exception?

--- about_nil.rb
+++ about_nil.rb
@@ -17,7 +17,7 @@ class AboutNil < Neo::Koan
 
       # What message was attached to the exception?
       # (HINT: replace __ with part of the error message.)
-      assert_match(/__/, ex.message)
+      assert_match(/undefined method/, ex.message)
     end
   end
 
