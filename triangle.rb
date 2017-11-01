2d1301d5fb531fe835bf1041b7e7a46b262c222a
Wilbur Suero <wilbur.himself@gmail.com>
2017-10-31 20:25:33 -0400

    complete triangle koan

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a.equal?(b) && b.equal?(c)
+    :equilateral
+  elsif a.equal?(b) || a.equal?(c) || b.equal?(c)
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
